a4dad67e6f434ebcd16cae50f8a95b747d62511e
berithx <seere7777@gmail.com>
2024-05-30 22:49:19 +0900

    미사용 파일 정리

--- src/main/java/com/homework/todo/repository/ImageRepository.java
+++ src/main/java/com/homework/todo/repository/ImageRepository.java
@@ -1,8 +1,8 @@
-package com.homework.todo.repository;
-
-import com.homework.todo.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-
-}
+//package com.homework.todo.repository;
+//
+//import com.homework.todo.entity.Image;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface ImageRepository extends JpaRepository<Image, Long> {
+//
+//}
--- src/main/java/com/homework/todo/service/ImageService.java
+++ src/main/java/com/homework/todo/service/ImageService.java
@@ -1,38 +1,38 @@
-package com.homework.todo.service;
-
-import com.homework.todo.entity.Image;
-import com.homework.todo.repository.ImageRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-
-@Service
-public class ImageService {
-    private final ImageRepository imageRepository;
-
-    private static final String UPLOAD_DIR = "imageUploads";
-
-    public ImageService(ImageRepository imageRepository) {
-        this.imageRepository = imageRepository;
-    }
-
-    public void savaImage(MultipartFile file) throws IOException {
-        String fileName = file.getOriginalFilename();
-        Path filepath = Paths.get(UPLOAD_DIR + fileName);
-
-        Files.createDirectories(filepath.getParent());
-
-        Files.copy(file.getInputStream(), filepath, StandardCopyOption.REPLACE_EXISTING);
-
-        Image image = new Image();
-        image.setFileName(fileName);
-        image.setFilePath(filepath.toString());
-
-        imageRepository.save(image);
-    }
-}
+//package com.homework.todo.service;
+//
+//import com.homework.todo.entity.Image;
+//import com.homework.todo.repository.ImageRepository;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.io.IOException;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.nio.file.StandardCopyOption;
+//
+//@Service
+//public class ImageService {
+//    private final ImageRepository imageRepository;
+//
+//    private static final String UPLOAD_DIR = "imageUploads";
+//
+//    public ImageService(ImageRepository imageRepository) {
+//        this.imageRepository = imageRepository;
+//    }
+//
+//    public void savaImage(MultipartFile file) throws IOException {
+//        String fileName = file.getOriginalFilename();
+//        Path filepath = Paths.get(UPLOAD_DIR + fileName);
+//
+//        Files.createDirectories(filepath.getParent());
+//
+//        Files.copy(file.getInputStream(), filepath, StandardCopyOption.REPLACE_EXISTING);
+//
+//        Image image = new Image();
+//        image.setFileName(fileName);
+//        image.setFilePath(filepath.toString());
+//
+//        imageRepository.save(image);
+//    }
+//}
